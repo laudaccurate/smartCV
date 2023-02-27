54c275b2f84eed2b28ad6050322d5512b89ef588
Laud Gilbert <laudaccurate@gmail.com>
2023-02-27 18:24:50 +0000

    displaying resume dta from ChatGPT

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { MantineProvider } from "@mantine/core";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Resume from "./components/Resume";
 
 const App = () => {
+  const [resumeData, setResumeData] = useState({});
+
   return (
     <MantineProvider>
       <div>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/resume" element={<Resume />} />
+            <Route path="/" element={<Home setResumeData={setResumeData} />} />
+            <Route
+              path="/resume"
+              element={<Resume resumeData={resumeData} />}
+            />
           </Routes>
         </BrowserRouter>
       </div>
--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -1,17 +1,84 @@
-import React from "react";
+import React, { useRef } from "react";
 import ErrorPage from "./ErrorPage";
+import { useReactToPrint } from "react-to-print";
 
-const Resume = ({ result }) => {
-  if (JSON.stringify(result) === "{}") {
+const Resume = ({ resumeData }) => {
+  const componentRef = useRef();
+
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+    documentTitle: `${resumeData.firstName + " " + resumeData.lastName} Resume`,
+    onAfterPrint: () => alert("Print Successful!"),
+  });
+
+  const replaceWithBr = (string) => {
+    return string.replace(/\n/g, "<br />");
+  };
+
+  if (JSON.stringify(resumeData) === "{}") {
     return <ErrorPage />;
   }
 
-  const handlePrint = () => alert("Print Successful!");
   return (
     <>
       <button onClick={handlePrint}>Print Page</button>
-      <main className="container">
-        <p>Hello!</p>
+      <main className="container" ref={componentRef}>
+        <header className="header">
+          <div>
+            <h1>{resumeData.fullName}</h1>
+            <p className="resumeTitle headerTitle">
+              {resumeData.currentPosition} ({resumeData.currentTechnologies})
+            </p>
+            <p className="resumeTitle">
+              {resumeData.currentLength}year(s) work experience
+            </p>
+          </div>
+          <div>
+            <img
+              src={resumeData.image_url}
+              alt={resumeData.fullName}
+              className="resumeImage"
+            />
+          </div>
+        </header>
+        <div className="resumeBody">
+          <div>
+            <h2 className="resumeBodyTitle">PROFILE SUMMARY</h2>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: replaceWithBr(resumeData.objective),
+              }}
+              className="resumeBodyContent"
+            />
+          </div>
+          <div>
+            <h2 className="resumeBodyTitle">WORK HISTORY</h2>
+            {resumeData.workHistory.map((work) => (
+              <p className="resumeBodyContent" key={work.name}>
+                <span style={{ fontWeight: "bold" }}>{work.name}</span> -{" "}
+                {work.position}
+              </p>
+            ))}
+          </div>
+          <div>
+            <h2 className="resumeBodyTitle">JOB PROFILE</h2>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: replaceWithBr(resumeData.jobResponsibilities),
+              }}
+              className="resumeBodyContent"
+            />
+          </div>
+          <div>
+            <h2 className="resumeBodyTitle">JOB RESPONSIBILITIES</h2>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: replaceWithBr(resumeData.keypoints),
+              }}
+              className="resumeBodyContent"
+            />
+          </div>
+        </div>
       </main>
     </>
   );
